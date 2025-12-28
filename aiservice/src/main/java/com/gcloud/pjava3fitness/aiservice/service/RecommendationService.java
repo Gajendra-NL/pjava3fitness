feb9a6f5489139db990fffe4e355241aa93c7146
Gajendra N L <gajendra.wbb@gmail.com>
2025-12-28 14:08:05 +0530

    ai service intiial commit

---
+++ aiservice/src/main/java/com/gcloud/pjava3fitness/aiservice/service/RecommendationService.java
@@ -0,0 +1,24 @@
+package com.gcloud.pjava3fitness.aiservice.service;
+
+import com.gcloud.pjava3fitness.aiservice.model.Recommendation;
+import com.gcloud.pjava3fitness.aiservice.repository.RecommendationRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class RecommendationService {
+    private final RecommendationRepository recommendationRepository;
+
+    public List<Recommendation> getUserRecommendation(String userId) {
+        return recommendationRepository.findByUserId(userId);
+    }
+
+    public Recommendation getActivityRecommendation(String activityId) {
+        return recommendationRepository.findByActivityId(activityId)
+                .orElseThrow(() -> new RuntimeException("No recommendation found for the activity " + activityId));
+    }
+}
